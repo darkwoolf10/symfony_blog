4f01672a7dba0ac2ee6f79f00d81b357e2716191
Pasha Fud'ko <woolftreuser@ukr.net>
2018-02-28 21:03:05 +0200

    added a counter of posts for the day

--- src/WoolfBundle/Controller/SubscribeController.php
+++ src/WoolfBundle/Controller/SubscribeController.php
@@ -5,42 +5,26 @@ namespace WoolfBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class SubscribeController extends Controller
 {
 
     /**
-     * @Security("is_granted('IS_AUTHENTICATED_FULLY')")
-     * @Route("add/subscribe/{username}", name="subscribe")
+     * @Route("/count")
      */
-    public function subscribeAction($username)
+    public function counterPostsAction()
     {
-        $em = $this->getDoctrine()->getManager();
-        $user = $em->getRepository('WoolfBundle:User')->findOneBy(['username' => $username]);
-
-        if(!$user) {
-            return new JsonResponse(['result' => 'failed']);
-        }
-
-        $currentUser = $this->getUser();
-
-        if(in_array($user->getEmail(), $currentUser->getSubscribe()) == false ) {
-            $currentUser->addSubscribe($user->getEmail());
-
-            $em->persist($currentUser);
-            $em->flush();
-
-            $email = $user->getEmail();
-
-            return new JsonResponse([
-                'result' => true,
-                'description' => 'Subsribe on ' . $username,
-                'email' => $email,
-            ]);
-        }
-
-        return new JsonResponse(['result' => 'failed']);
+        $today = date('Y-m-d H:i:s');
+        $yesterday = date('Y-m-d H:i:s',strtotime('-1 day'));
+        $em    = $this->getDoctrine()->getManager();
+        $query = $em->createQuery(
+            'SELECT COUNT(post)
+             FROM WoolfBundle:Post post
+             WHERE post.updated
+             BETWEEN :yesterday AND :today'
+        )->setParameter('today', $today)->setParameter('yesterday', $yesterday);
+        $count = $query->getResult();
+        return new JsonResponse($count);
     }
 }
---
+++ src/WoolfBundle/Repository/PostRepository.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: woolf
+ * Date: 27.02.18
+ * Time: 18:42
+ */
+
+namespace WoolfBundle\Repository;
+
+
+class PostRepository
+{
+
+}
